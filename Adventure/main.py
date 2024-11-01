537ae63d43bc0283a471c35bbf64c746f674d942
Spectator86 <167618825+Spectator86@users.noreply.github.com>
2024-11-01 21:17:15 +0300

    Update main.py
    Updated camera control

--- Adventure/main.py
+++ Adventure/main.py
@@ -1,55 +1,86 @@
+from colors import WHITE
+from physics import *
+from anim import *
 import pygame as pg
 
-from physics import PhysicBody
-from settings import *
-from player import Player
-from colors import *
+path = "images/HERO/anim_IDLE/"
+path1 = "images/HERO/eyes_anim/"
 
-pg.init()
-sc = pg.display.set_mode((WIDTH, HEIGHT))
+anim_list = list()
 
-clock = pg.time.Clock()
-count = 0
+for i in range(6):
+    anim_list.append(f"{path}anim{i+1}.png")
 
-eyes_anim = True
+anim = Anim(anim_list, 15)
 
-scroll_x = 0
-scroll_y = 0
-box_scale = 100
+eyes_anim_list = list()
 
-player = Player(WIDTH//2, 0, "images/HERO/anim_IDLE/anim1.png","images/HERO/eyes_anim/eyes1.png",  100, 0)
-object = PhysicBody(100, HEIGHT//2, "images/hero.png", 100, 0)
+for i in range(4):
+    eyes_anim_list.append(f"{path1}eyes{i+1}.png")
 
-while True:
-    sc.fill(GREEN)
+eyes_anim_list.append(f"{path1}eyes4.png")
+eyes_anim_list.append(f"{path1}eyes3.png")
+eyes_anim_list.append(f"{path1}eyes2.png")
+eyes_anim_list.append(f"{path1}eyes1.png")
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            exit()
+for i in range(20):
+    eyes_anim_list.append(f"{path1}eyes1.png")
 
-    if player.rect.y >= floor:
-        count = 0
+anim1 = Anim(eyes_anim_list, 15)
 
-    object.draw(sc, scroll_x, scroll_y)
-    object.gravity_down()
+timer = 0
 
-    player.draw(sc, scroll_x, scroll_y)
-    player.move(10, 25)
-    player.gravity_down()
-    player.animator()
-    player.eyes_draw(sc, scroll_x, scroll_y)
-    if eyes_anim:
-        player.eyes_animator()
+class Player(PhysicBody):
+    def __init__(self, x, y, file, eyes, scale, angle):
+        super().__init__(x, y, file,  scale, angle)
+        self.eyes_image = pg.image.load(eyes).convert_alpha()
+        self.eyes_image = pg.transform.scale(self.eyes_image, (scale, scale))
+        self.count = 0
+        self.direction = " "
+        self.cooldown = 60
+        self.timer = self.cooldown
+        self.dash_len = 200
+        self.dash = True
+        self.double_jump = True
 
-    if player.rect.x+player.scale//2 > scroll_x+box_scale+WIDTH//2:
-        scroll_x+=10
-    elif player.rect.x < scroll_x-box_scale+WIDTH//2:
-        scroll_x-=10
+    def eyes_draw(self, sc, scroll_x, scroll_y):
+        sc.blit(self.eyes_image, (self.rect.x-scroll_x, self.rect.y+10-scroll_y))
 
-    if player.rect.y+player.scale//2 > scroll_y+box_scale+HEIGHT//2:
-        scroll_y+=10
-    elif player.rect.y < scroll_y-box_scale+HEIGHT//2:
-        scroll_y-=10
+    def draw(self, sc, scroll_x, scroll_y):
+        sc.blit(self.image, (self.rect.x-scroll_x, self.rect.y-scroll_y))
+        if self.timer < self.cooldown:
+            pg.draw.rect(sc, WHITE, (self.rect.x+self.scale//2-scroll_x, self.rect.y - 20-scroll_y, self.timer, 10))
+            pg.draw.rect(sc, WHITE, ((self.rect.x+self.scale//2)-self.timer-scroll_x, self.rect.y - 20-scroll_y, self.timer, 10))
 
-    pg.display.update()
-    clock.tick(FPS)
+    def move(self, speed, jump) -> None:
+        keys = pg.key.get_pressed()
+        if keys[pg.K_d]:
+            self.rect.x += speed
+            self.direction = "right"
+
+        if keys[pg.K_a]:
+            self.rect.x -= speed
+            self.direction = "left"
+
+        if keys[pg.K_e] and self.dash:
+            if self.timer >= self.cooldown:
+                if self.direction == "right":
+                    self.rect.x += self.dash_len
+                    self.timer = 0
+
+                if self.direction == "left":
+                    self.rect.x -= self.dash_len
+                    self.timer = 0
+        if self.timer < self.cooldown:
+            self.timer += 1
+
+        if keys[pg.K_SPACE] and self.rect.y >= floor:
+            self.resistance = jump
+
+    def animator(self) -> None:
+        self.image = anim.update_anim()
+        self.image = pg.transform.scale(self.image, (self.scale, self.scale))
+
+    def eyes_animator(self):
+        self.eyes_image = anim1.update_anim()
+        self.eyes_image = pg.transform.scale(self.eyes_image, (self.scale, self.scale))
