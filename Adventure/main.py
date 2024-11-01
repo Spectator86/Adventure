c1b6b6f63f4b752e45b185a5afa17c9554bbd25c
Spectator86 <167618825+Spectator86@users.noreply.github.com>
2024-11-01 20:56:36 +0300

    Update main.py
    Added camera control in main.py

--- Adventure/main.py
+++ Adventure/main.py
@@ -13,6 +13,10 @@ count = 0
 
 eyes_anim = True
 
+scroll_x = 0
+scroll_y = 0
+box_scale = 100
+
 player = Player(WIDTH//2, 0, "images/HERO/anim_IDLE/anim1.png","images/HERO/eyes_anim/eyes1.png",  100, 0)
 object = PhysicBody(100, HEIGHT//2, "images/hero.png", 100, 0)
 
@@ -26,16 +30,26 @@ while True:
     if player.rect.y >= floor:
         count = 0
 
-    object.draw(sc)
+    object.draw(sc, scroll_x, scroll_y)
     object.gravity_down()
 
-    player.draws(sc)
+    player.draw(sc, scroll_x, scroll_y)
     player.move(10, 25)
     player.gravity_down()
     player.animator()
-    player.eyes_draw(sc)
+    player.eyes_draw(sc, scroll_x, scroll_y)
     if eyes_anim:
         player.eyes_animator()
+
+    if player.rect.x+player.scale//2 > scroll_x+box_scale+WIDTH//2:
+        scroll_x+=10
+    elif player.rect.x < scroll_x-box_scale+WIDTH//2:
+        scroll_x-=10
+
+    if player.rect.y+player.scale//2 > scroll_y+box_scale+HEIGHT//2:
+        scroll_y+=10
+    elif player.rect.y < scroll_y-box_scale+HEIGHT//2:
+        scroll_y-=10
 
     pg.display.update()
     clock.tick(FPS)
